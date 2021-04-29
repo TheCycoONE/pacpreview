07ae47920bc474982b699d2baf44cb9c7889bbad
Stephen E. Baker <baker.stephen.e@gmail.com>
2021-04-28 23:02:58 -0400

    Add a touch of colour to the output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 use alpm::{Alpm, AlpmList, Db, Dep, Package, SigLevel};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+fn print_installed(out: &mut impl WriteColor) -> std::io::Result<()> {
+    out.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+    write!(out, "[installed]")?;
+    out.reset()?;
+    Ok(())
+}
+
+fn print_outdated(out: &mut impl WriteColor) -> std::io::Result<()> {
+    out.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
+    write!(out, "[~installed]")?;
+    out.reset()?;
+    Ok(())
+}
 
 fn main() {
     let mut args = std::env::args();
@@ -15,68 +30,92 @@ fn main() {
             .unwrap();
     }
 
-    print_pkg_with_name(&pkg_name, &alpm);
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    let res = print_pkg_with_name(&mut stdout, &pkg_name, &alpm);
+    if let Err(e) = res {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
 
-fn print_pkg_with_name(pkg_name: &str, alpm: &Alpm) {
+fn print_pkg_with_name(
+    out: &mut impl WriteColor,
+    pkg_name: &str,
+    alpm: &Alpm,
+) -> std::io::Result<()> {
     let installed_pkg = alpm.localdb().pkg(pkg_name);
     let db_list = alpm.syncdbs();
     for db in db_list {
         if let Ok(pkg) = db.pkg(pkg_name) {
-            print_package_details(alpm, &db, &pkg, &installed_pkg.ok());
+            print_package_details(out, alpm, &db, &pkg, &installed_pkg.ok())?;
             break;
         }
     }
+    Ok(())
 }
 
-fn print_dep_list(alpm: &Alpm, dep_list: AlpmList<Dep>) {
+fn print_dep_list(
+    out: &mut impl WriteColor,
+    alpm: &Alpm,
+    dep_list: AlpmList<Dep>,
+) -> std::io::Result<()> {
     for dep in dep_list {
-        print!("    {}", dep.name());
+        write!(out, "    {}", dep.name())?;
         if let Some(ver) = dep.version() {
-            print!(" {}", ver);
+            write!(out, " {}", ver)?;
         }
         if !dep.desc().is_empty() {
-            print!(": {}", dep.desc());
+            write!(out, ": {}", dep.desc())?;
         }
         let ip = alpm.localdb().pkgs().find_satisfier(dep.to_string());
         if let Some(p) = ip {
+            write!(out, " ")?;
             if p.name() == dep.name() {
-                print!(" [installed]");
+                print_installed(out)?;
             } else {
-                print!(" [satisfied by {}]", p.name());
+                write!(out, " [satisfied by {}]", p.name())?;
             }
         }
-        println!();
+        writeln!(out)?;
     }
+    Ok(())
 }
 
-fn print_package_details(alpm: &Alpm, db: &Db, pkg: &Package, installed_pkg: &Option<Package>) {
-    print!("{}/{} {}", db.name(), pkg.name(), pkg.version());
+fn print_package_details(
+    out: &mut impl WriteColor,
+    alpm: &Alpm,
+    db: &Db,
+    pkg: &Package,
+    installed_pkg: &Option<Package>,
+) -> std::io::Result<()> {
+    write!(out, "{}/{} {}", db.name(), pkg.name(), pkg.version())?;
     if let Some(ip) = installed_pkg {
+        write!(out, " ")?;
         if ip.version() != pkg.version() {
-            print!(" [~installed]");
+            print_outdated(out)?;
         } else {
-            print!(" [installed]");
+            print_installed(out)?;
         }
     }
-    println!();
+    writeln!(out)?;
     if let Some(desc) = pkg.desc() {
-        println!();
-        println!("{}", desc);
+        writeln!(out)?;
+        writeln!(out, "{}", desc)?;
     }
-    println!();
+    writeln!(out)?;
     if let Some(ip) = installed_pkg {
         if ip.version() != pkg.version() {
-            println!("Installed Version: {}", ip.version());
+            writeln!(out, "Installed Version: {}", ip.version())?;
         }
         let reason = match ip.reason() {
             alpm::PackageReason::Depend => "dependency",
             alpm::PackageReason::Explicit => "explicit",
         };
-        println!("Installed Reason: {}", reason);
+        writeln!(out, "Installed Reason: {}", reason)?;
     }
-    println!("Opt Depends:");
-    print_dep_list(alpm, pkg.optdepends());
-    println!("Depends:");
-    print_dep_list(alpm, pkg.depends());
+    writeln!(out, "Opt Depends:")?;
+    print_dep_list(out, alpm, pkg.optdepends())?;
+    writeln!(out, "Depends:")?;
+    print_dep_list(out, alpm, pkg.depends())?;
+    Ok(())
 }
